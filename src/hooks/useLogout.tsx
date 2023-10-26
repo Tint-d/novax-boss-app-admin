809b76270e93a2ab5e1401b4f092b447d62370c8
thwe thwe tun <thwethwehtun2000@gmail.com>
2023-10-26 14:20:14 +0630

    minor: LogoutAlert added

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -1,13 +1,28 @@
+/* eslint-disable react-refresh/only-export-components */
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
+import Swal, { SweetAlertResult } from "sweetalert2";
 
 export default () => {
   const navigate = useNavigate();
 
   return () => {
-    Cookies.remove("token");
-    Cookies.remove("user");
-
-    navigate("/login");
+    Swal.fire({
+      title: "Are you sure?",
+      background: "#212121",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      iconColor: "#DCA715",
+      showCancelButton: true,
+      confirmButtonColor: "#166534",
+      cancelButtonColor: "#991B1B",
+      confirmButtonText: "Yes, log out!",
+    }).then((result: SweetAlertResult) => {
+      if (result.isConfirmed) {
+        Cookies.remove("token");
+        Cookies.remove("user");
+        navigate("/login");
+      }
+    });
   };
 };

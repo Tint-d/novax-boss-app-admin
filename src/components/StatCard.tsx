17e436288e2c03d73295a12b07706a8b79dc1fc3
thwe thwe tun <thwethwehtun2000@gmail.com>
2023-10-26 12:59:23 +0630

    minor: StatCard width fixed

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -5,7 +5,7 @@ interface PropsType {
 }
 const StatCard = ({ name, quantity }: PropsType) => {
   return (
-    <div className="bg-tableBgColor w-[200px] h-[120px] rounded-[20px] mb-8 flex flex-col items-center justify-center">
+    <div className="bg-tableBgColor w-[250px] h-[120px] rounded-[20px] mb-8 flex flex-col items-center justify-center">
       <p className="text-[25px] text-textColor">{name}</p>
       <p className="text-[30px] text-warining">{quantity}</p>
     </div>
